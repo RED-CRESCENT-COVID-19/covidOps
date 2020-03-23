a54534526de02b421cf03a2c8cd23ab13a4315c7
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-23 23:31:42 +0500

    adding react-redux, react-router adding initial state, config the init store & provided it to the router

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
+
+import configureStore from "./src/store";
 import AppNavigator from "./src/navigation/AppNavigator";
+
+const initialState = {};
+const store = configureStore(
+  initialState
+  //history
+);
+
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
