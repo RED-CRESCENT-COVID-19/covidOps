f5b0f5c3d327fb9368b345fe073553de9dfdfdde
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-23 23:16:03 +0500

    adding actions, reducers, constants & different packages etc

---
+++ src/reducers/admin/constants.js
@@ -0,0 +1,9 @@
+/*
+ * Admin Constants
+ */
+
+export const ADD_FIRST_CODE = "ADD_FIRST_CODE";
+export const ADD_SECOND_CODE = "ADD_SECOND_CODE";
+export const VERIFY_CODE = "VERIFY_CODE";
+export const HANDLE_ERROR = "HANDLE_ERROR";
+export const IS_LOADING = "IS_LOADING";
