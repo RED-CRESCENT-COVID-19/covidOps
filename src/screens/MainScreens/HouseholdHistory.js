cddac39d3fc33b759ad2b2cef52cb0d91e384c3c
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-26 18:10:23 +0500

    adding info list on home history list & i18n

--- src/screens/MainScreens/HouseholdHistory.js
+++ src/screens/MainScreens/HouseholdHistory.js
@@ -1,37 +1,77 @@
 import React, { Component } from "react";
-import {StyleSheet, View } from "react-native";
-
+import { StyleSheet, View, ScrollView } from "react-native";
 
 import { RaisedTextButton } from "react-native-material-buttons";
+
+// plugins
+import I18n from "../../plugins/I18n";
+
 //Custom Components
-import {CardView,Heading} from "../../components";
+import { CardView, Heading } from "../../components";
+import InfoList from "../../components/InfoList";
+
 //Theme
 import { Strings, Styles, Colors } from "../../../theme";
 
+const DATA = [
+  {
+    name: "Rizwan",
+    cnic: "4352345245",
+    id: "435224434565"
+  },
+  {
+    name: "Zaheer",
+    cnic: "4352256745",
+    id: "43522ds45"
+  },
+  {
+    name: "Ahmed",
+    cnic: "435wer2245",
+    id: "435223424522"
+  },
+  {
+    name: "faysal",
+    cnic: "4352342245",
+    id: "435223424542"
+  },
+  {
+    name: "fazlo",
+    cnic: "4352342245",
+    id: "435223424512"
+  },
+  {
+    name: "adfadf",
+    cnic: "435224234325",
+    id: "4352223445"
+  }
+];
 export default class HouseholdHistory extends Component {
-  
   handleDone = () => {
     this.props.navigation.goBack();
   };
   render() {
     return (
       <View style={Styles.container}>
-      <Heading headerText={Strings.headings.HOUSEHOLDHISTORY} />
-      <CardView Styles={Styles.Spacer300} />
-      <CardView Styles={Styles.Spacer200} />
+        <Heading headerText={I18n.t(`headings.HOUSEHOLDHISTORY`)} />
+        {/* <CardView Styles={Styles.Spacer300} /> */}
+        <CardView Styles={Styles.Spacer50} />
+        <ScrollView style={Styles.ScrollView}>
+          {DATA.map(d => (
+            <InfoList data={d} key={d.id} {...this.props} />
+          ))}
+        </ScrollView>
 
-      <View style={Styles.rightButtonContainer}>
-
-        <RaisedTextButton
-          title={Strings.ButtonTitles.DONE}
-          color={Colors.primaryColor}
-          titleColor={Colors.buttonTextColor}
-          shadeBorderRadius={1.5}
-          style={Styles.smallButton}
-          onPress={this.handleDone}
-        />
+        <View style={Styles.rightButtonContainer}>
+          <RaisedTextButton
+            title={I18n.t(`ButtonTitles.DONE`)}
+            color={Colors.primaryColor}
+            titleColor={Colors.buttonTextColor}
+            shadeBorderRadius={1.5}
+            style={Styles.smallButton}
+            onPress={this.handleDone}
+          />
+        </View>
       </View>
-    </View>
     );
   }
 }
