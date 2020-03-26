5a0b5fe22333ee578dcb5efe2f7314e8c9b6da38
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-26 20:51:53 +0500

    Complete the precautions screen missing styling, translation, move question list data in precautions/information screen in to language file for support i18n

--- src/screens/MainScreens/Precautions.js
+++ src/screens/MainScreens/Precautions.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, ScrollView } from "react-native";
 
 import { OutlinedTextField } from "react-native-material-textfield";
 import { RaisedTextButton } from "react-native-material-buttons";
@@ -8,19 +8,75 @@ import { RaisedTextButton } from "react-native-material-buttons";
 import I18n from "../../plugins/I18n";
 
 //Custom Components
-import Heading from "../../components/Heading";
-import CardView from "../../components/CardView";
+import { Heading, CardView } from "../../components";
+
 //Theme
-import { Strings, Styles, Colors } from "../../../theme";
+import { Styles, Colors } from "../../../theme";
+
+const QUESTIONS_LIST = I18n.t(`QUESTIONS_LIST.Precautions`);
 
 export default class Precautions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeQuestion: 0
+    };
+  }
+
+  onNextButton() {
+    const { activeQuestion } = this.state;
+    if (activeQuestion < QUESTIONS_LIST.length - 1)
+      this.setState({ activeQuestion: activeQuestion + 1 });
+    else {
+      this.props.navigation.navigate("Information");
+    }
+  }
+  onBackButton() {
+    const { activeQuestion } = this.state;
+    if (activeQuestion > 0)
+      this.setState({ activeQuestion: activeQuestion - 1 });
+    else {
+      this.props.navigation.goBack();
+    }
+  }
   render() {
+    const { activeQuestion } = this.state;
+    console.log("activeQuestion state is: ", activeQuestion);
+    console.log("QUESTIONS_LIST.length is: ", QUESTIONS_LIST.length);
+
     return (
       <View style={Styles.container}>
         <Heading headerText={I18n.t(`headings.PRECAUTIONS`)} />
         <Text style={Styles.topParagraph}>
           {I18n.t(`Paragarphs.PRECAUTIONS`)}
         </Text>
+        {/* <CardView Styles={Styles.Spacer50} /> */}
+        <Text style={Styles.topQuestion}>
+          {QUESTIONS_LIST[activeQuestion].Question}
+        </Text>
+        <ScrollView style={Styles.ScrollView}>
+          <Text style={[Styles.answer, { textAlign: "center" }]}>
+            {QUESTIONS_LIST[activeQuestion].Answer}
+          </Text>
+        </ScrollView>
+        <View style={Styles.buttonsContainer}>
+          <RaisedTextButton
+            title={I18n.t(`ButtonTitles.BACK`)}
+            color={Colors.secondaryColor}
+            titleColor={Colors.buttonTextColor}
+            shadeBorderRadius={1.5}
+            style={Styles.smallButton}
+            onPress={() => this.onBackButton()}
+          />
+          <RaisedTextButton
+            title={I18n.t(`ButtonTitles.NEXT`)}
+            color={Colors.primaryColor}
+            titleColor={Colors.buttonTextColor}
+            shadeBorderRadius={1.5}
+            style={Styles.smallButton}
+            onPress={() => this.onNextButton()}
+          />
+        </View>
       </View>
     );
   }
