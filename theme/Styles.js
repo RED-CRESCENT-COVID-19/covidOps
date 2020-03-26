d5fd1201053db346a2f02426a5be79be0739cb65
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-26 20:58:25 +0500

    clean up code

--- theme/Styles.js
+++ theme/Styles.js
@@ -97,34 +97,6 @@ const Styles = StyleSheet.create({
   Spacer200: {
     paddingTop: 200
   },
-  // containerHeader: {
-  //   paddingTop: 95,
-  //   paddingLeft: 32,
-  //   paddingRight: 32
-  // },
-  // textHeader: {
-  //   fontSize: 34,
-  //   color: Colors.primaryColor
-  // },
-  // lineHeader: {
-  //   height: 2,
-  //   backgroundColor: Colors.primaryColor,
-  //   alignSelf: "stretch"
-  // },
-  // lineDivider: {
-  //   height: 1,
-  //   backgroundColor: Colors.dividerColor,
-  //   alignSelf: "stretch",
-  //   paddingLeft: 20,
-  //   paddingRight: 20
-  // },
-  // topParagraph: {
-  //   paddingTop: 20,
-  //   paddingLeft: 36,
-  //   paddingRight: 36,
-  //   fontSize: 16,
-  //   color: Colors.paragraphTextColor
-  // },
   genderText: {
     paddingTop: 20,
     paddingLeft: 36,
@@ -132,32 +104,10 @@ const Styles = StyleSheet.create({
     fontSize: 16,
     color: Colors.primaryColor
   },
-  // Spacer100: {
-  //   paddingTop: 100
-  // },
-  // Spacer200: {
-  //   paddingTop: 200
-  // },
+
   Spacer250: {
     paddingTop: 250
   },
-  // Spacer300: {
-  //   paddingTop: 300
-  // },
-  // leftButtonContainer: {
-  //   paddingRight: 220,
-  //   paddingLeft: 20
-  // },
-  // rightButtonContainer: {
-  //   paddingLeft: 220,
-  //   paddingRight: 20
-  // },
-  // smallButton: {
-  //   width: 140,
-  //   height: 44,
-  //   overflow: "hidden",
-  //   borderRadius: 40
-  // },
   smallGenderButton: {
     width: 106,
     height: 36,
@@ -175,17 +125,6 @@ const Styles = StyleSheet.create({
     paddingLeft: 35,
     paddingRight: 35
   },
-  // buttonsContainer: {
-  //   flex: 1,
-  //   flexDirection: "row",
-  //   justifyContent: "space-between",
-  //   padding: 20,
-  //   position: "absolute",
-  //   bottom: 20,
-  //   // height: 100,
-  //   width: "100%"
-  // },
-
   infoItemContainer: {
     paddingTop: 10,
     paddingLeft: 35,
