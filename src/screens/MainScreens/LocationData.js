bc1a522d97aeaa6662e9e87e3fe96506b58bfe51
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2020-03-28 19:30:08 +0500

    adding access location & it's logic

--- src/screens/MainScreens/LocationData.js
+++ src/screens/MainScreens/LocationData.js
@@ -3,6 +3,10 @@ import { Text, StyleSheet, View } from "react-native";
 
 import { RaisedTextButton } from "react-native-material-buttons";
 
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+
 // plugins
 import I18n from "../../plugins/I18n";
 
@@ -13,16 +17,66 @@ import CardView from "../../components/CardView";
 import { Strings, Styles, Colors } from "../../../theme";
 const WRITING_STYLE = I18n.locale;
 export default class LocationData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: "",
+      lng: "",
+      errorMessage: null
+    };
+  }
+
+  UNSAFE_componentWillMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+  _getLocationAsync = async () => {
+    try {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      console.log("status is: ", status);
+      if (status !== "granted") {
+        this.setState({
+          errorMessage: "Permission to access location was denied"
+        });
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log("get location is: ", location);
+      this.setState({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    } catch (error) {
+      console.log("catch error is: ", error);
+    }
+  };
   handleContinue = () => {
     this.props.navigation.navigate("HouseholdNumber");
   };
+
   render() {
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
+    console.log("render location is: ", this.props.location);
+
     const style = WRITING_STYLE === "ur" ? { writingDirection: "rtl" } : {};
+
     return (
       <View style={Styles.container}>
         <Heading headerText={I18n.t(`headings.LOCATIONDATA`)} />
         <Text style={[Styles.topParagraph, style]}>
           {I18n.t(`Paragarphs.LOCATIONDATA`)}
+          lat: {this.state.lat} + lng: {this.state.lng}
         </Text>
         <CardView Styles={Styles.Spacer300} />
         <CardView Styles={Styles.Spacer100} />
